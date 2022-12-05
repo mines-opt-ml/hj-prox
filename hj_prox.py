4bbb2b75ff24ff480a227ff214424920eb7a36de
Howard Heaton <54549555+howardheaton@users.noreply.github.com>
2022-12-04 20:49:25 -0800

    Added hj-prox algorithm

---
+++ hj_prox.py
@@ -0,0 +1,62 @@
+import torch
+import numpy as np
+
+def compute_hj_prox(x, t, f, delta=1e-1, int_samples=1000, alpha=2.0,
+                    recursion_depth=0, alpha_decay=0.631, tol=1.0e-6, 
+                    tol_underflow=0.9, device='cpu', verbose=False):
+    ''' Estimate proximals from function value sampling via HJ-Prox Algorithm.
+
+        Notes:
+            Input is a single vector "x" of size (dim, 1)    
+
+            Proximal computation does *not* handle case for f with negative
+            values. That case must be handled by adding an offset and is
+            omitted her since all our functions of interest are nonnegative.
+
+            The computation for the proximal involves the exponential of a
+            potentially large negative number, which can result in underflow
+            in floating point arithmetic that renders a grossly inaccurate 
+            proximal calculation. To avoid this, the "large negative number" 
+            is reduced in size by using a smaller value of alpha, returning 
+            a result once the underflow is not considered significant
+            (as defined by the tolerances "tol" and "tol_underflow").
+    '''
+    valid_vector_shape = x.shape[1] == 1 and x.shape[0] >= 1
+    assert valid_vector_shape
+
+    recursion_depth +=1
+    std_dev  = np.sqrt(delta * t / alpha)
+    dim      = x.shape[0]
+    y        = std_dev * torch.randn(int_samples, dim, device=device) 
+    y        = y + x.permute(1,0)
+    exp_term = torch.exp(-f(y) * (alpha / delta)) 
+
+    underflow = exp_term <= tol
+    underflow_freq = float(underflow.sum()) / underflow.shape[0]
+    observe_underflow = underflow_freq > tol_underflow
+
+    if observe_underflow: 
+        alpha *= alpha_decay
+        return compute_hj_prox(x, t, f, delta=delta, int_samples=int_samples,
+                               alpha=alpha, recursion_depth=recursion_depth,
+                               alpha_decay=alpha_decay, tol=tol, 
+                               tol_underflow=tol_underflow, device=device,
+                               verbose=verbose)         
+    else:
+        v_delta   = torch.mean(exp_term)
+        numerator = y * exp_term.view(int_samples, 1)
+        numerator = torch.mean(numerator, dim=0)        
+        prox_term = numerator / v_delta
+        HJ_prox   = prox_term.view(-1, 1) 
+
+        valid_prox_shape = HJ_prox.shape == x.shape
+        assert valid_prox_shape
+
+        prox_is_finite = (HJ_prox < np.inf).all()
+        assert prox_is_finite 
+
+        if verbose:
+            envelope = - (delta / alpha) * torch.log(v_delta)
+            return HJ_prox, recursion_depth, envelope
+        else:
+            return HJ_prox
