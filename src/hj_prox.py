a8255388a2edc4334e9c18b22ad79e6c60823a68
Howard Heaton <54549555+howardheaton@users.noreply.github.com>
2023-04-06 20:27:58 -0700

    Update hj_prox.py

--- src/hj_prox.py
+++ src/hj_prox.py
@@ -8,7 +8,9 @@ def compute_hj_prox(x, t, f, delta=1e-1, int_samples=1000, alpha=2.0,
     """ Estimate proximals from function value sampling via HJ-Prox Algorithm.
 
         The output computes
-        $ prox_{tf}(x) = f(y) + \frac{1}{2t} | y - x |^2, $
+        .. math::
+            prox_{tf}(x) = f(y) + \frac{1}{2t} | y - x |^2,
+            
         where $\mathsf{x}$ = `x` is the input, $\mathsf{t}$=`t` is the time parameter, 
         and $\mathsf{f}$=`f` is the function of interest.
         The process for this approximation is as follows.
