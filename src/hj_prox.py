5184f7e6974d51e6f6947bb9c21d3e1914584aed
Howard Heaton <54549555+howardheaton@users.noreply.github.com>
2023-04-06 20:47:52 -0700

    Update hj_prox.py

--- src/hj_prox.py
+++ src/hj_prox.py
@@ -10,7 +10,7 @@ def compute_hj_prox(x, t, f, delta=1e-1, int_samples=1000, alpha=2.0,
         The output estimates the proximal:
         
         $$
-            f(y) + \frac{1}{2t} | y - x |^2,
+            f(y) + \dfrac{1}{2t} | y - x |^2,
         $$
             
         where $\mathsf{x}$ = `x` is the input, $\mathsf{t}$=`t` is the time parameter, 
